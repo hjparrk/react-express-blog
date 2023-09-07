aa58eef87798193877cf6c6c267aa68eeb56d1ed
Hyojoon Park <hyojoon2372552@naver.com>
2023-09-07 12:40:36 +1000

    implement auth apis

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,27 @@
 import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { loginUserAPI } from "../apis/userAPI";
 
 const loginUser = async (username, password) => {
   const response = await loginUserAPI(username, password);
-  const data = response.data;
-  console.log(data);
+  const data = await response.data;
+  return data;
 };
 
 const Login = () => {
   const usernameRef = useRef();
   const passwordRef = useRef();
 
+  const navigation = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const username = usernameRef.current.value;
     const password = passwordRef.current.value;
-    loginUser(username, password);
+    const response = loginUser(username, password);
+    console.log(response);
+
+    navigation("/");
   };
 
   return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { registerUserAPI } from "../apis/userAPI";
+
+const registerUser = async (username, password, confirmPassword) => {
+  const response = await registerUserAPI(username, password, confirmPassword);
+  const data = await response.data;
+  console.log(data);
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (password === confirmPassword) {
+      registerUser(username, password, confirmPassword);
+      navigate("/login");
+    }
+  };
+
+  return (
+    <div>
+      <form className="flex flex-col w-24">
+        <label form="username">username</label>
+        <input
+          className="border-2 border-black"
+          id="username"
+          ref={usernameRef}
+        />
+        <label form="password">password</label>
+        <input
+          className="border-2 border-black"
+          id="password"
+          ref={passwordRef}
+        />
+        <label form="confirm-password">username</label>
+        <input
+          className="border-2 border-black"
+          id="confirm-password"
+          ref={confirmPasswordRef}
+        />
+        <button onClick={handleSubmit}>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
