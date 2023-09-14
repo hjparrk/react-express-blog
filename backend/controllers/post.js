980b238cd759177234f29dbcd8d71f3729152bdf
Hyojoon Park <hyojoon2372552@naver.com>
2023-09-15 04:21:52 +1000

    add view all posts API

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -36,21 +36,28 @@ async function store(req, res) {
 
 async function view(req, res) {
 
-    const post = await prisma.post.findUnique({
-        where: {
-            id: 4
-        }
-    })
+    const {postId} = req.params
 
+    try {
+        const post = await prisma.post.findUnique({
+            where: {
+                id: +postId
+            }
+        })
 
-    return res.status(200).json(post)
+        return res.status(200).json(post)
+    } catch (error) {
+        return res.status(404).json({message: error.message})
+    }
 }
 
 async function viewAll(req,res) {
+    const posts = await prisma.post.findMany()
+    return res.status(200).json(posts)
 }
 
 async function update(req, res) {}
 
 async function destroy(req, res) {}
 
-module.exports = { store, view, update, destroy };
+module.exports = { store, view, viewAll, update, destroy };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,7 +3,8 @@ const postController = require("../controllers/post")
 
 const router = express.Router()
 
-router.post("/posts", postController.store)
+router.post("/posts/", postController.store)
+router.get("/posts/", postController.viewAll)
 router.get("/posts/:postId", postController.view)
 
 module.exports = router;
